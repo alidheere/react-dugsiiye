72f6c40c0c37b54f3ee699040e13ec5eedbe7588
alidheere <sakajr33@gmail.com>
2026-08-20 11:13:41 -0700

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,4 @@
-// import { useContext } from "react";
-// import { Link, NavLink,  useNavigate } from "react-router-dom";
-// import { AuthContext } from "./login page/Authcontext";
-
-// import { Outlet } from "react-router-dom";
-
-
-
-
-// function App() {
-// const {isAuthencated,logout}= useContext(AuthContext)
-// const navigate= useNavigate()
-// const hadleLogout=()=>{
-//   logout()
-//   navigate()
-// }
-//   return (
-// <div className=" h-screen  mx-auto mt-5 p-5 bg-white rounded-lg shadow-md" >
-
-
-  
-// <nav  className="flex space-x-4 bg-yellow-50 items-center justify-center">
-// <NavLink to="/"  className="font-bold"> home</NavLink>
-//   {isAuthencated ? (
-//     <>
-//       <NavLink to="/create" className="  font-bold hover:bg-blue-600 text-white p-2">Create Post</NavLink>
-
-//       <button onClick={hadleLogout} className=" font-bold hover:bg-red-500 text-white p-2">Logout</button>
-//     </>
-//   ) : (
-//     <NavLink to="/login">Login</NavLink>
-//   )}
-// </nav>
-
-// <Outlet/> 
-
-
-// </div>
-//   );
-// }
-// export default App;
+;
 
 import { useContext } from "react";
 import { NavLink, useNavigate, Outlet } from "react-router-dom";
